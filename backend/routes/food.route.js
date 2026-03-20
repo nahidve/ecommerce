becfd5ce9991a4a6606c22b6c3d107c444b86800
nahidve <nahidazad@virtualemployee.com>
2026-03-20 17:05:10 +0530

    admin food remove resolved

--- backend/routes/food.route.js
+++ backend/routes/food.route.js
@@ -2,7 +2,6 @@ import express from "express"
 import { addFood, listFood, removeFood, rateFood } from "../controllers/food.controller.js"
 import multer from "multer"
 import authMiddleware from "../middleware/auth.middleware.js"
-import adminMiddleware from "../middleware/admin.middleware.js"
 
 const foodRouter = express.Router()
 
@@ -19,19 +18,22 @@ const upload = multer({ storage: storage})
 
 //@desc Add Food
 //@route POST /api/food/add
-//@access Private
+//@access Admin
 foodRouter.post("/add",  upload.single("image"), addFood) 
 
 //@desc List Foods
 //@route GET /api/food/list
-//@access Public
+//@access Admin
 foodRouter.get("/list", listFood)
 
 //@desc Remove Food
 //@route DELETE /api/food/remove
-//@access Private
-foodRouter.delete("/remove", authMiddleware, adminMiddleware, removeFood)
+//@access Admin
+foodRouter.delete("/remove",  removeFood)
 
+//@desc Rating Food
+//@route RATE /api/food/rate
+//@access Admin
 foodRouter.post("/rate", authMiddleware, rateFood)
 
 export default foodRouter
